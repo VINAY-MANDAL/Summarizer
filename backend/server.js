1be40ce9b0ea4a319fdb1671f83496318eb21bc1
VINAY-MANDAL <vinaykumarmandal847227@example.com>
2026-05-31 06:23:36 +0530

    fix trust proxy

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const pdfRoutes = require("./routes/pdf");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set("trust proxy", 1);
 app.use(cors({ origin: "*", methods: ["GET", "POST"] }));
 app.use(express.json({ limit: "2mb" }));
 
